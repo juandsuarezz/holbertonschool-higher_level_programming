35b908a69d7775e410e21a4d5a08aca618e0ff44
juandsuarezz <1486@holbertonschool.com>
2020-04-27 22:52:41 +0000

    3 update

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first3 = word[0:3]
+last2 = word[-2:]
+middle = word[1:-1]
+print("First 3 letters: {}".format(first3))
+print("Last 2 letters: {}".format(last2))
+print("Middle word: {}".format(middle))
