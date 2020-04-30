305ae1f5ed6fafecc0e51022753283c4f7ed0bf5
juandsuarezz <1486@holbertonschool.com>
2020-04-30 20:43:35 +0000

    pep8 style corrected

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     sum = 0
-    
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         sum = sum + int(argv[i])
-    
     print(sum)
