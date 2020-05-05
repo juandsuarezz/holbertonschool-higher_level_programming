4b5b68b6997e1acd77e8454e18290c163eb8a1a2
juandsuarezz <1486@holbertonschool.com>
2020-05-05 19:27:23 +0000

    Range fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    if idx < 0:
-        return new_list
-    elif idx >= len(my_list):
-        return new_list
-    else:
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
         new_list[idx] = element
         return list_copy
