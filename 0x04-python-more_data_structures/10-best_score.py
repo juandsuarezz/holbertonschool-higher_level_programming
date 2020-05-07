db192bedfd0856c2a18ef809c8cde9a7bf6ef01d
juandsuarezz <1486@holbertonschool.com>
2020-05-07 18:13:04 +0000

    First update

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        return sorted(a_dictionary.keys())[-1]
+    return None
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for i in a_dictionary:
+                new_dictionary[i] = a_dictionary[i] * 2
+    return new_dictionary
