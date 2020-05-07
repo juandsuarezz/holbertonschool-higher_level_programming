385dc8942f5d3a50b81f9486f0d9cfc588850ea3
juandsuarezz <1486@holbertonschool.com>
2020-05-07 18:21:15 +0000

    Conditional Fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    else:
         return sorted(a_dictionary.keys())[-1]
-    return None
