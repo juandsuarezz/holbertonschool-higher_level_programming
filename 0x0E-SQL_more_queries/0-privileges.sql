afefd669da0803458f0404c3db6b176b783e64b6
juandsuarezz <1486@holbertonschool.com>
2020-07-14 19:20:38 +0000

    [Update]: sql files

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of user_0d_1 and user_0d_2
+-- lists all privileges of user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all privileges of user_0d_2
 SHOW GRANTS FOR user_0d_2@localhost;
