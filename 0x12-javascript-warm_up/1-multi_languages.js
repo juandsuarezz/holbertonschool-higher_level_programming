f9f2a701a9b7389c12b73c963943238485828732
juandsuarezz <1486@holbertonschool.com>
2020-09-15 18:16:27 +0000

    [Add]: js files

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Prints 3 lines
+
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'Javascript is amazing';
+
+console.log(firstline + '\n' + secondline + '\n' +
+                thirdline);
